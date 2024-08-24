3a4cce2efd2f746d73264294019d846c3e092c6c
Garvit Maloo <garvitmaloo.21@gmail.com>
2024-08-24 21:57:24 +0530

    Fix build error

--- src/store/slices/counterSlice.ts
+++ src/store/slices/counterSlice.ts
@@ -1,15 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { PayloadAction } from "@reduxjs/toolkit/dist/createAction";
-
-import { CounterActionPayload } from "../../types";
 
 const counterSlice = createSlice({
   name: "counter",
   reducers: {
     increaseByOne: (state) => state + 1,
     decreaseByOne: (state) => state - 1,
-    increaseByNumber: (state, action: PayloadAction<CounterActionPayload>) =>
-      state + action.payload.number,
   },
   initialState: 0,
 });
